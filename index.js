bcd57c8d457c71275d25cf0b9e6f2043ed6650da
martin <martin@comets.kr>
2024-01-31 22:52:44 +0900

    fix: addion

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cluster = require("cluster");
 const instaceCPUs = require("os").cpus();
 
+console.log("HELLO");
+
 if (cluster.isMaster) {
   console.log(`MASTER thread via process id => ${process.pid}`);
 
